2ae601dd9688175e5d98bd922998c42c67ebffca
Mike Boan <mikeboan@gmail.com>
2018-04-07 15:29:57 -0400

    Update example to use has_one_through

--- example.rb
+++ example.rb
@@ -7,9 +7,15 @@ class Artist < SQLObject
 end
 
 class Album < SQLObject
-
   belongs_to :band,
     foreign_key: :artist_id,
     class_name: "Artist"
 
+  has_many :songs
+end
+
+class Song < SQLObject
+  belongs_to :album
+
+  has_one_through(:artist, :album, :band)
 end
